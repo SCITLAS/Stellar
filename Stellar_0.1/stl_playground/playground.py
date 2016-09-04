efe11fc8701d8780a5168b0bc241e2728e394653
Li Yuhuang <liyuhuangjojo@163.com>
2016-09-04 22:39:43 +0800

    Integrate RQAlpha test back frame

--- Stellar_0.1/stl_playground/playground.py
+++ Stellar_0.1/stl_playground/playground.py
@@ -407,6 +407,39 @@ def contextmanager_test():
 
 
 
+
+def matplotlib_test():
+    # import matplotlib.pyplot as plot
+    # for index, color in enumerate('rgbyck'):
+    #     plot.subplot(321+index, axisbg=color)
+    # plot.show()
+
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    x = np.linspace(-4, 4, 200)
+    f1 = np.power(10, x)
+    f2 = np.power(np.e, x)
+    f3 = np.power(2, x)
+
+    plt.plot(x, f1, 'r', x, f2, 'b', x, f3, 'g', linewidth=2)
+    plt.axis([-4, 4, -0.5, 8])
+    plt.text(1, 7.5, r'$10^x$', fontsize=16)
+    plt.text(2.2, 7.5, r'$e^x$', fontsize=16)
+    plt.text(3.2, 7.5, r'$2^x$', fontsize=16)
+    plt.title('A simple example', fontsize=20)
+
+    plt.savefig('power.png', dpi=75)
+    plt.show()
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
     # dataframe2csv_test()
     # dataframe2list_test()
@@ -434,6 +467,10 @@ if __name__ == '__main__':
     # do_ts_coroutine_test()
 
     # wrap_test()
-    contextmanager_test()
+    # contextmanager_test()
+
+    matplotlib_test()
+
+
 
 
